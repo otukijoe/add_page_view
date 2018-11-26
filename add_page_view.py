8da87442cd6bf85fe1da788bf1a13011dd4d9b1a
otuki <woxingqiao@163.com>
2018-11-26 16:37:04 +0800

    主程序

--- add_page_view.py
+++ add_page_view.py
@@ -8,14 +8,13 @@
 """
 
 import requests
-# import random
 import config
 import get_useragent
-# import get_ip_proxy
+import get_ip_proxy
 from time import sleep
 count = 0
 
-def get_html(rate):
+def get_html(good_proxies, rate):
     global count
     useragent = get_useragent.get_user_agent()
     header = {
@@ -25,10 +24,10 @@ def get_html(rate):
         'Cookie': config.cookie,
     }
     print 'The current user-agent is: ', useragent
-    # proxies = random.choice(config.proxies)
-    # print 'The current proxy IP is: ',proxies
-    # res = requests.get(config.url, headers=header, proxies=proxies)
-    res = requests.get(config.url, headers=header)
+    proxies = get_ip_proxy.get_random_ip(good_proxies)
+    print 'The current proxy IP is: ', proxies
+    res = requests.get(config.url, headers=header, proxies=proxies)
+    # res = requests.get(config.url, headers=header)
     if res is not None and res.status_code == 200:
         # print res.content
         count = count + 1
@@ -37,15 +36,16 @@ def get_html(rate):
         sleep(rate)
 
 if __name__ == '__main__':
+   #访问频率：秒
     rate = input('please input the rate of visit(unit:second):')
     print 'start:'
     sleep(1)
     for i in range(3, 0, -1):
         print i
         sleep(1)
+    good_proxies = get_ip_proxy.get_proxies()
     while True:
         try:
-            get_html(rate)
+            get_html(good_proxies, rate)
         except Exception as e:
             print e
-
